23798ffb06cc995301de191ec356eba0fc4a5bc0
zzw <zzw@163.com>
2019-03-02 17:45:18 +0800

    fast commit

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ["google", "prettier"],
+  parserOptions: { ecmaVersion: 2017 },
+};
---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const router = require('koa-router')()
+const config=require('config')
+
+router.prefix('/users')
+
+router.post('/register', function (ctx, next) {
+  console.log('+++config');
+  console.log(config)
+  ctx.body = 'this is a users response!'
+})
+
+router.get('/bar', function (ctx, next) {
+  ctx.body = 'this is a users/bar response'
+})
+
+module.exports = router
